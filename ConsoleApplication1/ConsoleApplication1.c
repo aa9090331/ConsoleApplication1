ed64cb7af0ba298aa4ab1127cad51f54033e9082
weide Liu <omeskdles@yahoo.com.tw>
2022-04-11 01:54:15 +0800

    加入專案檔案。

---
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<math.h>
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning( disable : 4996 )
+
+
+int fun(int n) {
+	int result = 0, f1 = 1, f2 = 1;
+	if (n <= 0)	return 0;
+	if (n == 1 || n==2)	return 1;
+
+	for (int i = 3; i <= n; i++) {
+		result = f1 + f2;
+		f1 = f2;
+		f2 = result;
+	}
+	return result;
+}
+
+int main(void) {
+	printf("%d", fun(3));
+	return 0;
+}
